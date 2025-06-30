218de6492a2c1b7523fe8036a71a70f3978dceda
ethancancode <ethan.rodrics@gmail.com>
2025-06-30 16:39:51 +0530

    Added changes to sidebar and made it responsive

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const menuButton = document.getElementById("menubar_button")
 const sidebar = document.getElementById("sidebar_container");
-const pageContent = document.querySelector(".video-gallery");
 
 let isOpen = false;
 
-    menuButton.addEventListener('click', () => {
-      isOpen = !isOpen;
-      if (isOpen) {
-        sidebar.classList.add('open');
-        pageContent.classList.add('shift');
-      } else {
-        sidebar.classList.remove('open');
-        pageContent.classList.remove('shift');
-      }
-    });
+menuButton.addEventListener('click', () => {
+  if (isOpen) {
+    sidebar.classList.remove('open');
+    sidebar.classList.add('close');
+  } else {
+    sidebar.classList.add('open');
+    sidebar.classList.remove('close');
+  }
+  isOpen = !isOpen;
+});
